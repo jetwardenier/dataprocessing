9aae4cb9e965e58704bc3090d98cdc8c5220dd66
jetwardenier <43347798+jetwardenier@users.noreply.github.com>
2019-05-21 16:53:19 +0200

    Delete OvChipkaartDao.java

--- OvChipkaartDao.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface OvChipkaartDao {
-	public List<OvChipkaart> findByReiziger(int reizigerId);
-	public OvChipkaart save(OvChipkaart ovChipkaart);
-	public OvChipkaart update(OvChipkaart ovChipkaart);
-	public boolean delete(OvChipkaart ovChipkaart);
-}
